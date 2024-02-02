58e439fd71244d1728965acc2f0241d66956d254
danicode <danielanzawa@gmail.com>
2024-02-02 17:16:02 -0300

    Creando librería commons y generando jar usando maven

--- src/main/java/com/servicio/commons/CommonsApplication.java
+++ src/main/java/com/servicio/commons/CommonsApplication.java
@@ -1,13 +1,7 @@
 package com.servicio.commons;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CommonsApplication.class, args);
-	}
-
 }
