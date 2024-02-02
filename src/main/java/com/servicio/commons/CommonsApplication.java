27b3c413526cc74308b180e2f46c7268ba47eb82
danicode <danielanzawa@gmail.com>
2024-02-02 18:00:15 -0300

    Modificando servicio productos para agregar dependencia commons

--- src/main/java/com/servicio/commons/CommonsApplication.java
+++ src/main/java/com/servicio/commons/CommonsApplication.java
@@ -1,7 +1,10 @@
 package com.servicio.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class CommonsApplication {
 }
